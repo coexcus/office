df949be2aff9e9d1c16095dcf0473e0fc83244eb
sksar <soumyakantisar@gmail.com>
2023-06-29 21:01:27 +0530

    Updates

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	},
 	preprocess: vitePreprocess()
 };
-export default config;
